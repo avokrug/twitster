e0ec3a6d08c7de3869d0647cafaf61a9142f2f05
Aleksey <aleksey.gurkov.dev@gmail.com>
2019-08-31 20:30:23 +0300

    Initial commit

---
+++ src/main/java/com/twister/twister/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.twister.twister.repos;
+
+import com.twister.twister.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
