1211de3a911f1c3fd2b0f28417e9f0c1c48818ea
Aleksey <aleksey.gurkov.dev@gmail.com>
2019-09-01 16:28:12 +0300

    change mustache on thymeleaf

--- src/main/java/com/twister/twister/controller/MainController.java
+++ src/main/java/com/twister/twister/controller/MainController.java
@@ -1,23 +1,26 @@
 package com.twister.twister.controller;
 
 import com.twister.twister.domain.Message;
+import com.twister.twister.domain.User;
 import com.twister.twister.repos.MessageRepo;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
 public class MainController {
-    @Autowired
-    private MessageRepo messageRepo;
+    private final MessageRepo messageRepo;
+
+    public MainController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
     @GetMapping("/")
-    public String greeting(Map<String, Object> model) {
+    public String greeting() {
         return "greeting";
     }
 
@@ -28,9 +31,9 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
-        Message message = new Message(text, tag);
+    @PostMapping("add")
+    public String add(@AuthenticationPrincipal User user, @RequestParam String text, @RequestParam String tag, Map<String, Object> model){
+        Message message = new Message(text, tag, user);
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
--- src/main/java/com/twister/twister/domain/Role.java
+++ src/main/java/com/twister/twister/domain/Role.java
@@ -1,5 +1,12 @@
 package com.twister.twister.domain;
 
-public enum  Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum  Role implements GrantedAuthority {
     USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
