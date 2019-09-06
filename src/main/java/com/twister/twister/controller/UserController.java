23a2b3937d8af8793db206c69ef9c72aed76e818
Aleksey <aleksey.gurkov.dev@gmail.com>
2019-09-06 16:51:27 +0300

    add security

--- src/main/java/com/twister/twister/controller/UserController.java
+++ src/main/java/com/twister/twister/controller/UserController.java
@@ -3,14 +3,19 @@ package com.twister.twister.controller;
 import com.twister.twister.domain.Role;
 import com.twister.twister.domain.User;
 import com.twister.twister.repos.UserRepo;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     private final UserRepo userRepo;
 
@@ -30,4 +35,20 @@ public class UserController {
         model.addAttribute("roles", Role.values());
         return "userEdit";
     }
+
+    @PostMapping
+    public String userChangeRole(@RequestParam String username, @RequestParam List<String> user_roles, @RequestParam("userId") User user){
+        user.setUsername(username);
+        Set<String> roles = Arrays.stream(Role.values()).map(Role::name).collect(Collectors.toSet());
+        user.getRoles().clear();
+        for (String user_role : user_roles) {
+            if(roles.contains(user_role)){
+                user.getRoles().add(Role.valueOf(user_role));
+            }
+        }
+        userRepo.save(user);
+        return "redirect:/user";
+    }
+
+
 }
